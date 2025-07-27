3aaa0e8f86ac75836b4490c3e66ce2157554bd17
Brendon <bluicien@yahoo.com>
2025-07-27 18:54:39 +1200

    Added additional data for projects and fixed github links

--- src/pages/projects/data.ts
+++ src/pages/projects/data.ts
@@ -26,21 +26,28 @@ const projects: Array<Project> = [
         name: "Seeding Tool & AI-Powered Search",
         description: "Engineered a CLI seeding tool with dual interactions using classic CLI commands and modern menu options. Also integrated an AI powered keyword search with NLP capabilities.",
         imgUrl: "",
-        repoUrl: "https://github.com/MR-2025-PT-March-L5ADV/mission-5-phase-1-individual-bluicien"
+        repoUrl: "https://github.com/bluicien/cli-seeder-ai-search.git"
     },
     {
         id: 4,
         name: "School E-Learning Platform",
         description: "Collaborated in a development team to build a cross-school online learning platform for students to work on projects and teachers to mark and grade results.",
         imgUrl: "",
-        repoUrl: "https://github.com/Mission-Ready/2407-L4PT04-missionx-frontend-t3"
+        repoUrl: "https://github.com/bluicien/e-school-learning.git"
     },
     {
         id: 5,
+        name: "AI Insurance Consultant",
+        description: "Chatbot powered by Gemini AI. It enables the user to get personalized car insurance recommendations.",
+        imgUrl: "",
+        repoUrl: "https://github.com/bluicien/AI-insurance-consultant.git"
+    },
+    {
+        id: 6,
         name: "AI Interview Chat Bot",
         description: "Chatbot powered by Gemini AI. It enables the user to type any job profession of choice to simulate a job interview.",
         imgUrl: "",
-        repoUrl: "https://github.com/bluicien/ai-job-interview-chatbot"
+        repoUrl: "https://github.com/bluicien/AI-practice-job-interview.git"
     },
     {
         id: 6,
