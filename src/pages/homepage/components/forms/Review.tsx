b95b2b57431e13c576995a83e2e347619c79c5d5
Brendon <bluicien@yahoo.com>
2025-10-27 17:34:42 +1300

    Added review posting feature

--- src/pages/homepage/components/forms/Review.tsx
+++ src/pages/homepage/components/forms/Review.tsx
@@ -5,13 +5,16 @@ import { ImStarFull } from "react-icons/im";
 
 type Message = {
     name: string;
+    company?: string;
+    position?: string;
+    ipAddress?: string;
     rating: number;
     review: string;
 }
 
 export default function Review() {
-    const [ userReview, setUserReview ] = useState<Message>({ name: "", rating: 0, review: "" });
-    const [starHovered, setStarHovered] = useState<number>(0)
+    const [ userReview, setUserReview ] = useState<Message>({ name: "", rating: 0, review: "", position: "", company: "" });
+    const [ starHovered, setStarHovered ] = useState<number>(0)
 
     const handleKeyStroke = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
         e.preventDefault();
@@ -35,13 +38,48 @@ export default function Review() {
         }
     }
 
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
+        e.preventDefault();
+        console.log(userReview);
+        try {
+            const response = await fetch(`${import.meta.env.VITE_BACKEND_API}/api/feedback`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(userReview),
+            });
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            console.log('Success:', data);
+            alert('Thank you for your review!');
+            setUserReview({ name: "", rating: 0, review: "", position: "", company: "" });
+        }
+        catch (error) {
+            console.error('Error submitting review:', error);
+            alert('There was an error submitting your review. Please try again later.');
+        }
+    }
+
     return (
-        <form className={styles.formField} >
+        <form className={styles.formField} onSubmit={handleSubmit} >
             <fieldset className={styles.field} >
                 <legend className={styles.formTitle} >Review</legend>
-                <label className={styles.formLabel} htmlFor="name">Name: </label>
+                <label className={styles.formLabel} htmlFor="name">Name* : </label>
                 <input className={styles.formInput} type="text" name="name" id="name" value={userReview.name} onChange={handleKeyStroke} />
-                <label className={styles.formLabel} htmlFor="review">Rating: </label>
+                <div className={styles.inlineInputs} >
+                    <div className={styles.inlineFields} >
+                        <label className={styles.formLabel} htmlFor="company">Company : </label>
+                        <input className={styles.formInput} type="text" name="company" id="company" value={userReview.company} placeholder="Not Required" onChange={handleKeyStroke} />
+                    </div>
+                    <div className={styles.inlineFields} >
+                        <label className={styles.formLabel} htmlFor="position">Position : </label>
+                        <input className={styles.formInput} type="text" name="position" id="position" value={userReview.position} placeholder="Not Required" onChange={handleKeyStroke} />
+                    </div>
+                </div>
+                <label className={styles.formLabel} htmlFor="review">Rating* : </label>
                 <div>
                 {Array.from({ length: 5 }).map((_, index) => (
                     <ImStarFull
@@ -51,15 +89,10 @@ export default function Review() {
                         onMouseEnter={() => setStarHovered(index + 1)}
                         onMouseLeave={() => setStarHovered(0)}
                     />
-                ))}                    
-                {/* <ImStarFull color={(userReview.rating >= 1 || starHovered >= 1) ? 'gold' : ''} onClick={() => handleClickStar(1)} onMouseEnter={() => setStarHovered(1)} onMouseLeave={() => setStarHovered(0)} />
-                <ImStarFull color={(userReview.rating >= 2 || starHovered >= 2) ? 'gold' : ''} onClick={() => handleClickStar(2)} onMouseEnter={() => setStarHovered(2)} onMouseLeave={() => setStarHovered(0)} />
-                <ImStarFull color={(userReview.rating >= 3 || starHovered >= 3) ? 'gold' : ''} onClick={() => handleClickStar(3)} onMouseEnter={() => setStarHovered(3)} onMouseLeave={() => setStarHovered(0)} />
-                <ImStarFull color={(userReview.rating >= 4 || starHovered >= 4) ? 'gold' : ''} onClick={() => handleClickStar(4)} onMouseEnter={() => setStarHovered(4)} onMouseLeave={() => setStarHovered(0)} />
-                <ImStarFull color={(userReview.rating >= 5 || starHovered >= 5) ? 'gold' : ''} onClick={() => handleClickStar(5)} onMouseEnter={() => setStarHovered(5)} onMouseLeave={() => setStarHovered(0)} /> */}
+                ))}
                 </div>
                 <input type="text" hidden />
-                <label className={styles.formLabel} htmlFor="review">Review: </label>
+                <label className={styles.formLabel} htmlFor="review">Review : </label>
                 <textarea className={styles.textBox} name="review" id="review" value={userReview.review} onChange={handleKeyStroke} ></textarea><br />
                 <button className={styles.formBtn} >Leave Review!</button>
             </fieldset>
